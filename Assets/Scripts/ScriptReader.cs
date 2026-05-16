9019db67e0e4cf868c7b619bc0233eccc126ec16
AriBlank <arusa668@gmail.com>
2026-05-17 01:36:51 +0300

    add cocktail system and some plot

--- Assets/Scripts/ScriptReader.cs
+++ Assets/Scripts/ScriptReader.cs
@@ -6,27 +6,52 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
-public class ScriptsReader: MonoBehaviour
+public class ScriptsReader : MonoBehaviour
 {
 
     [SerializeField] private TextAsset InkJSONAsset;
     [SerializeField] private TMP_Text CharacterName;
     [SerializeField] private TMP_Text DialogueBox;
     [SerializeField] private GameObject TutorialPanel;
+    [SerializeField] private GameObject CockPanel;
+    [Header("Параметри коктейля")]
+    private int strength = 0;
+    private int sweetness = 0;
+    private int sourness = 0;
+    private int bitterness = 0;
+    private int spiciness = 0;
+    private bool ice = false;
+    private int totalIngredientsPoints = 0;
 
-    private bool IsTutorial = false;
+
+    [Header("UI елементи")]
+    public TMP_Text pointsCount;
+    public TMP_Text strengthCount;
+    public TMP_Text sweetnessCount;
+    public TMP_Text sournessCount;
+    public TMP_Text bitternessCount;
+    public TMP_Text spicinessCount;
+    public TMP_Text iceStatusText;
+    public Button serveButton;
+    public Button resetButton;
+
     private bool isTutorialOpen = false;
+    private bool isMixing = false;
     public Story story;
     void Start()
     {
         story = new Story(InkJSONAsset.text);
         story.ChoosePathString("begining");
         ShowNextLine();
-        TutorialPanel.SetActive(IsTutorial);
-    }               
+        TutorialPanel.SetActive(isTutorialOpen);
+        CockPanel.SetActive(isMixing);
+        UpdateUI();
+    }
     public void ShowNextLine()
     {
         if (isTutorialOpen) return;
+        if (isMixing) return;
+
         if (!story.canContinue)
         {
             DialogueBox.text = "the end";
@@ -55,6 +80,10 @@ public class ScriptsReader: MonoBehaviour
                     CharacterName.text = "";
                     isTutorialOpen = true;
                 }
+                if (tag.Trim() == "start_making_cock")
+                {
+                    StartCocktailMaking();
+                }
 
                 continue;
             }
@@ -73,6 +102,117 @@ public class ScriptsReader: MonoBehaviour
         TutorialPanel.SetActive(false);
         isTutorialOpen = false;
     }
+    public void StartCocktailMaking()
+    {
+        isMixing = true;
+        CockPanel.SetActive(true);
+        DialogueBox.text = "";
+        CharacterName.text = "";
+
+    }
+    public void StrengthIncrease()
+    {
+        if (strength < 5)
+        {
+            strength++;
+            UpdateUI();
+        }
+    }
+    public void SweetnessIncrease()
+    {
+        if (totalIngredientsPoints < 5)
+        {
+            sweetness++;
+            totalIngredientsPoints++;
+            UpdateUI();
+        }
+    }
+    public void SournessIncrease()
+    {
+        if(totalIngredientsPoints < 5)
+        {
+            sourness++;
+            totalIngredientsPoints++;
+            UpdateUI();
+        }
+    }
+    public void BitternessIncrease()
+    {
+        if(totalIngredientsPoints < 5)
+        {
+            bitterness++;
+            totalIngredientsPoints++ ;
+            UpdateUI();
+        }
+    }
+    public void SpicinessIncrease()
+    {
+        if(totalIngredientsPoints < 5 && spiciness < 3)
+        {
+            spiciness++;
+            totalIngredientsPoints++;
+            UpdateUI();
+        }
+    }
+    public void IsIceExist()
+    {
+        ice = !ice;
+        UpdateUI();
+    }
+    public void ServeCock()
+    {
+        if (GetTotalPoints() == 0) return;
+        CocktailData cocktail = new CocktailData
+        {
+            strength = strength,
+            sweetness = sweetness,
+            sourness = sourness,
+            bitterness = bitterness,
+            spiciness = spiciness,
+            ice = ice
+        };
+        OnCocktailServed( cocktail );
+        CockPanel.SetActive(false);
+        isMixing = false;
+        ResetCock();
+    }
+    private int GetTotalPoints()
+    {
+        return strength + totalIngredientsPoints;
+    }
+    public void ResetCock()
+    {
+        strength = 0;
+        sweetness = 0;
+        sourness = 0;
+        bitterness = 0;
+        spiciness = 0;
+        ice = false;
+        totalIngredientsPoints = 0;
+
+        UpdateUI();
+    }
+    public void OnCocktailServed(CocktailData cocktail)
+    {
+        story.variablesState["cocktail_strength"] = cocktail.strength;
+        story.variablesState["cocktail_sweetness"] = cocktail.sweetness;
+        story.variablesState["cocktail_sourness"] = cocktail.sourness;
+        story.variablesState["cocktail_bitterness"] = cocktail.bitterness;
+        story.variablesState["cocktail_spiciness"] = cocktail.spiciness;
+        story.variablesState["cocktail_ice"] = cocktail.ice;
+
+        ShowNextLine();
+    }
+    private void UpdateUI()
+    {
+        strengthCount.text = strength.ToString();
+        sweetnessCount.text = sweetness.ToString();
+        sournessCount.text = sourness.ToString();
+        bitternessCount.text = bitterness.ToString();
+        spicinessCount.text = spiciness.ToString();
+        pointsCount.text = $"{GetTotalPoints()}/10";
+        iceStatusText.text = ice ? "Added" : "None";
+    }
     void Update()
     {
         if (Keyboard.current.spaceKey.wasPressedThisFrame) 
@@ -80,4 +220,15 @@ public class ScriptsReader: MonoBehaviour
             ShowNextLine(); 
         }
     }  
+}
+
+[System.Serializable]
+public class CocktailData
+{
+    public int strength;
+    public int sweetness;
+    public int sourness;
+    public int bitterness;
+    public int spiciness;
+    public bool ice;
 }
--- Assets/ink/Ink.ink
+++ Assets/ink/Ink.ink
@@ -1,3 +1,9 @@
+VAR cocktail_strength = 0
+VAR cocktail_sweetness = 0
+VAR cocktail_sourness = 0
+VAR cocktail_bitterness = 0
+VAR cocktail_spiciness = 0
+VAR cocktail_ice = false
 ===begining===
 Alright… Time to get back to work. It’s only been three days since the murder of our regular customer, Zack. The police only left around lunchtime today, and they still haven’t fully solved the case. On top of that, the manager got reprimanded because most of the security cameras turned out to be fake dummies, which is why the case remains unsolved. Hopefully, he’ll take improving the bar’s security seriously in the near future. #name: Miki
 
@@ -6,9 +12,45 @@ Since the manager decided to rebrand the bar, I need to go through the new cockt
 
 === tutorial ===
 #tutorial
-
+...
 ->first_client
 ===first_client===
-...
-hihihiiii #Mike
+\*sound of the door opening*\
+
+Yooooooooooo~ #name: ?
+Miki, hey there, sweetheart.#name: ?
+Same to you, John. What’ll you have to drink?#name: Miki
+Haven’t decided yet. Heard some guy got killed around here or something. So, how’s the investigation going? You keep talking while I figure out what to order from such a charming bartender~ #name: John
+Long story short, the case is currently on hold due to a lack of evidence. But as for what happened, our regular got shot. There’s not much we can do right now, so all that’s left is to keep working.#name: Miki
+\*I probably shouldn’t mention that the cameras were fake dummies…*\ #name: Miki
+\*I wouldn’t want to stay in a place where someone got killed and there are security issues on top of that.*\ #name: Miki
+\*Though now that I think about it, I’m still here… what a nightmare.*\ #name:Miki
+Mmm, I see. Well then, as they say: “May justice prevail, and may the culprit be found.#name: John
+This whole story inspired me to drink something strong, but smooth enough to go down easy.#name: John
+\*bro is insane, how can murders inspire someone*\ #name: Miki
+Coming right up. #name: Miki
+#start_making_cock
+{
+    - cocktail_strength == 5 and cocktail_sweetness == 2 and cocktail_sourness == 2:
+        -> secret_reaction_John
+    - cocktail_strength < 5 and cocktail_strength >= 3:
+        -> great_reaction_John
+    - else:
+        -> ordinary_reaction_John
+}
+===secret_reaction_John===
+You did it fucking well #name: John
+-> ending_John
+
+===great_reaction_John===
+yeah, it what i wanted #name:John
+-> ending_John
+
+===ordinary_reaction_John===
+So... In general, it's okay #name: John
+-> ending_John
+
+===ending_John===
+gre
 -> END
+
